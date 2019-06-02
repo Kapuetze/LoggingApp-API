ef51c37b696df5014711ef139fae8d5c64815e50
Knotenbaer <knotenbaer@gmail.com>
2019-06-02 17:57:53 +0200

    Ability to query for logs
    - passing a mongoose query object to the /search route will query for the values

--- routes/logs.js
+++ routes/logs.js
@@ -6,9 +6,8 @@ var Log = require(gBase_dir + '/models/log.js');
 
 /* CREATE a new log entry. */
 router.post('/', 
-passport.authenticate(['basic', 'all'], { session: false }),    
-function(req, res, next) {
-
+passport.authenticate(['basic', 'all'], { session: false }), 
+async (req, res, next) => {
 	// Creating one log.
 	var log = new Log ({
 		user: req.user._id,
@@ -24,22 +23,42 @@ function(req, res, next) {
 		} else {
 			res.json({ status: "success", message: "Log saved successfully."});
 		}
-	});
+    });
 });
 
 /* GET all logs. */
-router.get('/', 
-passport.authenticate(['basic', 'all'], { session: false }),    
-function(req, res, next) {
+// router.get('/', 
+// passport.authenticate(['basic', 'all'], { session: false }),    
+// function(req, res, next) {
 
-	//console.log(req.user);
-	Log.find({ user: req.user }, function (err, logs) {
-    if(err){
-      res.json({ status: "error", message: err});
-    }else{
-      res.json(logs);
-    }
-	});	
+// 	//console.log(req.user);
+// 	Log.find({ user: req.user }, function (err, logs) {
+//         if(err){
+//             res.json({ status: "error", message: err});
+//         }else{
+//             res.json(logs);
+//         }
+// 	});	
+// });
+
+/* SEARCH LOGS */
+router.post('/search', passport.authenticate(['basic', 'all'], {session: false}), async (req, res, next) => {
+    try {
+        query = {};
+        //build query
+        if(req.body !== null){
+            query = req.body;
+        }
+        query["user"] = req.user;
+
+        //get logs from database
+        var logs = await Log.find(query);
+
+        //output the logs to the response
+        res.json(logs);
+    } catch (error) {
+        res.status(500).json({ error: "Error fetching the logs from the server." });
+    }  
 });
 
 module.exports = router;
